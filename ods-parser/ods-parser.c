ffe68add577d0fc4898ea39f75be80025668ffda
Levi <2006leviryan@gmail.com>
2026-05-11 10:53:40 -0400

    factored out the unziping

--- ods-parser/ods-parser.c
+++ ods-parser/ods-parser.c
@@ -15,9 +15,17 @@
 #define DICT_PATH "Ankaso.ods"
 #define DICT_SHEET_NAME "Dictionary.xml"
 
-int main(void){
-	//TODO: factor out the zip archive to it's own function
-	int result = 0;
+//TODO: document .ods
+bool get_content(String_View* out){
+	/*
+	 * miniz is only used with this function
+	 * and it's a rather big thing for what we
+	 * so TODO: replace miniz with a smaller lib
+	 *
+	 * this is the unziper of ther zip
+	 * which also grabs the content.xml
+	 */
+	bool result = true;//defer
 	mz_zip_archive zip_archive;
 	memset(&zip_archive, 0, sizeof(zip_archive));
 
@@ -25,28 +33,39 @@ int main(void){
 	if (!status) {
 		//TODO: factor out ankaso.ods
 		printf("Could not open Ankaso.ods as zip file.\n");
-		defer(1);
+		defer(false);
 	}
 	
 	int file_i = mz_zip_reader_locate_file(&zip_archive,"content.xml",NULL,0);
 	if(file_i < 0){
 		//TODO: factor out sheet_name
 		printf("could not find content.xml in ankaso.ods\n");
-		defer(1);
+		defer(false);
 	}
 
 	size_t size;
+	//TODO: maybe this needs to use custom alocator
+	//or memcpy
 	char* pBuf = mz_zip_reader_extract_to_heap(&zip_archive, file_i, &size, 0);	
 	if(!pBuf){
 		//TODO: factor out content.xml
 		printf("failed to move content.xml to heap\n");
-		defer(1);
+		defer(false);
 	}
 	String_View sv = {.count = size,.data = pBuf};
-	printf(SV_Fmt,SV_Arg(sv));
+	*out = sv;
 defer:
-	//if the order matters then a memcp will need to be done
-	if(pBuf) mz_free(pBuf);
 	mz_zip_reader_end(&zip_archive);
+	return result;
+}
+
+int main(void){
+	int result = 0;//defer
+	String_View content;
+	if(!get_content(&content)) defer(1);
+	printf("count = %zu\n",content.count);
+defer:
+	//TODO: make this better
+	mz_free((void*)content.data);
 	return result;
 }
