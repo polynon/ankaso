cde91d6615cc51a415d863cc1bff9f11bc78f708
Levi <2006leviryan@gmail.com>
2026-05-16 13:49:11 -0400

    added miniz as a submodule

--- ods-parser/nob.c
+++ ods-parser/nob.c
@@ -7,8 +7,15 @@
 #define debug_flags "-fsanitize=address","-g"
 #define warnings "-Wextra","-Wall"// debug_flags
 
+bool compile_miniz(void){
+	Cmd cmd = {0};
+	//TODO("compile miniz");
+	return true;
+}
+
 int main(int argc, char **argv){	
 	GO_REBUILD_URSELF(argc,argv);
+	if(!compile_miniz()) return 1;
 	Cmd cmd = {0};
 	cmd_append(&cmd,cc,warnings,Olevel,"ods-parser.c","-o","ods-parser","-L./include/miniz/","-lminiz");
 	if(!cmd_run(&cmd)) return 1;
